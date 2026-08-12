bab3536f034c3702cfa9e4d7a614279c85ddee45
kim <kimyujin080807@gmail.com>
2026-08-12 14:53:16 +0900

    로그인 틀 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Main from './Component/Main'
+import MemberLogin from "./Component/member/MemberLogin";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <div>
             <Routes>
                 <Route path='/' element={<Main />}/>
+                <Route path='/memberLogin' element={<MemberLogin />}/>
             </Routes>
         </div>
     );
--- src/Component/member/MemberLogin.js
+++ src/Component/member/MemberLogin.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import '../../style/member/MemberLogin.css';
 
 function MemberLogin() {
     return (
-        <div>MemberLogin</div>
+        <div>
+            <div>
+                <div>
+                    <div>아이디</div>
+                    <div>
+                        <input type='text'/>
+                    </div>
+                </div>
+                <div>
+                    <div>비밀번호</div>
+                    <div>
+                        <input type='password'/>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <button>로그인</button>
+                    </div>
+                </div>
+                <div style={{display:'flex'}}>
+                    <div>아이디 찾기</div>|
+                    <div>비밀번호 찾기</div>
+                </div>
+                <div>
+                    <div>----------또는----------</div>
+                </div>
+                <div>
+                    <div>카카오 로그인</div>
+                </div>
+                <div>
+                    <div>아직 회원이 아니신가요?</div>
+                    <div>회원가입</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
